44f346b3e60c60f0d9527b4bc47b311d48eca093
rodri <rodrigojuanmari@gmail.com>
2025-07-03 12:56:18 -0300

    Agrego identificacion de cliente

--- app.js
+++ app.js
@@ -15,105 +15,139 @@ const sobreNosotros = "HXfc73d64a5f842aded7fac0af5d082fff"
 
 const noLlegó = "HX0cba42d4b6fdc98e57f029ad7df3b574"
 
+let presentacion = 1
+
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/webhook', async (req, res) => {
-    //const termino = 0;
+    let presentacion = 1
     const waId = req.body.WaId;
     const body = req.body.Body;
     const ListTitle = req.body.ListTitle;
     const title = req.body.ListTitle;
 
-    if (estados[waId]?.paso === 'esperando') {
-        const datos = estados[waId];
-        console.log("Observación recibida:", body);
-        
-        await prisma.reclamo.create({
-        data: {
-            cliente: waId,
-            tipo: datos.tipo,
-            area: datos.area,
-            cod_factura: datos.cod_factura,
-            estado: false,
-            observacion: body,
+
+    if (presentacion === 1) {
+        twilio.sendMessage(waId, "Bienvenido al asistente virtual de *Disroi*, mi nombre es *Rodri*\n\n" + "Para comenzar, le solicito que me brinde su *número de cliente* (sin ceros) o su *número de documento* para su correcta identificación\n\n");
+        //busqueda por codigo de cliente
+        if(body.length < 8){
+            let codigoCeros = body
+            for (let i = 0; i < 10 - body.length; i++) {
+                codigoCeros = '0' + codigoCeros;
+            }
+            const cliente = await prisma.maestro_cliente.findFirst({where: {codigo: codigoCeros}});
+            if (cliente){
+                twilio.sendMessage(waId, `Hola ${cliente.nombre}`);
+                presentacion = 0;
+            }
         }
-        });
-        delete estados[waId];
-        return;
-    }
-
-    //main
-    if (req.body.MessageType != 'interactive') {
-        
-
-        if(req.body.Body === 'lechuza') {
-            twilio.sendMessage(waId, "piashins piashins");
+        //busqueda por dni
+        else{
+            const dniIngresado = req.body.Body.trim();
+            const cliente = await prisma.$queryRaw`
+                SELECT * FROM maestro_cliente
+                WHERE dni = ${dniIngresado}
+                OR SUBSTRING_INDEX(SUBSTRING_INDEX(cuit, '-', 2), '-', -1) = ${dniIngresado}
+            `;
+            if (cliente) {
+                twilio.sendMessage(waId, `Hola ${cliente[0].nombre}`);
+                presentacion = 0;
+            }
         }
-        else
-            twilio.sendListPicker(waId, main);
+
+    }
+    else{
+        if (estados[waId]?.paso === 'guardar') {
+            const datos = estados[waId];
+            console.log("Observación recibida:", body);
+            
+            await prisma.reclamo.create({
+            data: {
+                cliente: "0000000021",
+                tipo: datos.tipo,
+                area: datos.area,
+                cod_factura: datos.cod_factura,
+                estado: false,
+                observacion: body,
+            }
+            });
+            delete estados[waId];
+            return;
+        }
+
+        //main
+        if (req.body.MessageType != 'interactive') {
+            
+            if(req.body.Body === 'lechuza') {
+                twilio.sendMessage(waId, "piashins piashins");
+            }
+            else
+                twilio.sendListPicker(waId, main);
+        }
+
+        //1er seccion
+        console.log("req ->", req.body);
+        switch (body) {
+            case 'Reclamos':
+                twilio.sendListPicker(waId, reclamo);
+                break;
+            case 'Pedidos':
+                twilio.sendListPicker(waId, pedido);
+                break;
+            case 'Acerca de nosotros':
+                twilio.sendListPicker(waId, sobreNosotros);
+                break;
+        }
+        //reclamos
+        switch (ListTitle) {
+            case 'No llegó mi pedido':
+                //pedir num de facturacion y cod cliente
+                twilio.sendListPicker(waId, noLlegó);
+                break;
+            case 'Me falta un producto':
+                //pedir num de facturacion y cod cliente
+                twilio.sendListPicker(waId, noLlegó);
+                break;
+            case 'Mi vendedor no me visitó':
+                //avisarle al vendedor directamente que hay que visitarlo de manera urgente
+                break;
+            case 'Diferencia en CC':
+                twilio.sendMessage(waId, "Por favor, escribe una observación sobre lo ocurrido. Cualquier información adicional es bienvenida.");
+                estados[waId] = {
+                    paso: 'guardar',
+                    tipo: ListTitle,
+                    cod_factura: 'FAC12345',
+                    area: 'Administracion'
+                };
+                break;
+            case 'Solicitud NDC':
+                //avisarle al vendedor directamente que no se hizo la nota de crédito
+                break;
+            case 'Requiero atención':
+                //debido a la alta demanda, no puedo asegurar que el supervisor se contacte contigo. Le recomiendo
+                //comunicarse con el vendedor
+                break;
+        }
+
+        switch (ListTitle) {
+            case 'Tokin':
+                twilio.sendMessage(waId, "Para llevar a cabo su pedido por *Tokin*, visite la tienda oficial de *Disroi*: https://www.tokintienda.com/");
+                break;
+            case 'Manual':
+                twilio.sendMessage(waId, "Para llevar a cabo su pedido de manera *Manual* puede comunicarse con su vendedor designado. ¿Requiere su contacto?");
+                break;
+            case 'Ver ofertas':
+                //Mostrar ofertas
+                break;
+        }
+        //volver al menu principal
+        switch (title) {
+            case 'Volver al menú anterior':
+                twilio.sendListPicker(waId, main);
+                break;
+        }
     }
 
-    //1er seccion
-    console.log("req ->", req.body);
-    switch (body) {
-        case 'Reclamos':
-            twilio.sendListPicker(waId, reclamo);
-            break;
-        case 'Pedidos':
-            twilio.sendListPicker(waId, pedido);
-            break;
-        case 'Acerca de nosotros':
-            twilio.sendListPicker(waId, sobreNosotros);
-            break;
-    }
-    //reclamos
-    switch (ListTitle) {
-        case 'No llegó mi pedido':
-            //pedir num de facturacion y cod cliente
-            twilio.sendListPicker(waId, noLlegó);
-            break;
-        case 'Me falta un producto':
-            //pedir num de facturacion y cod cliente
-            twilio.sendListPicker(waId, noLlegó);
-            break;
-        case 'Mi vendedor no me visitó':
-            //avisarle al vendedor directamente que hay que visitarlo de manera urgente
-            break;
-        case 'Diferencia en CC':
-            twilio.sendMessage(waId, "Por favor, escribe una observación sobre lo ocurrido. Cualquier información adicional es bienvenida.");
-            estados[waId] = {
-                paso: 'esperando',
-                tipo: ListTitle,
-                cod_factura: 'FAC12345',
-                area: 'Administracion'
-            };
-            break;
-        case 'Solicitud NDC':
-            //avisarle al vendedor directamente que no se hizo la nota de crédito
-            break;
-        case 'Requiero atención':
-            //debido a la alta demanda, no puedo asegurar que el supervisor se contacte contigo. Le recomiendo
-            //comunicarse con el vendedor
-            break;
-    }
-
-    switch (ListTitle) {
-        case 'Tokin':
-            twilio.sendMessage(waId, "Para llevar a cabo su pedido por *Tokin*, visite la tienda oficial de *Disroi*: https://www.tokintienda.com/");
-            break;
-        case 'Manual':
-            twilio.sendMessage(waId, "Para llevar a cabo su pedido de manera *Manual* puede comunicarse con su vendedor designado. ¿Requiere su contacto?");
-            break;
-        case 'Ver ofertas':
-            //Mostrar ofertas
-            break;
-    }
-    //volver al menu principal
-    switch (title) {
-        case 'Volver al menú anterior':
-            twilio.sendListPicker(waId, main);
-            break;
-    }
 });
 
 app.listen(3000, () => {
